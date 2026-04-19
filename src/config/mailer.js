ce9ce920ab4feb56e355e76dbfa135f763432fd5
DragutuMatei <mateidr7@gmail.com>
2026-04-19 19:08:01 +0300

    sss

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -3,6 +3,9 @@ import nodemailer from 'nodemailer';
 const transporter = nodemailer.createTransport({
     service: "gmail",
     secure: false,
+    pool: true,
+    maxConnections: 1,
+    maxMessages: 100,
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
